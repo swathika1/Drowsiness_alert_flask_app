975b89add008ca094311a444bd65b607b8b04143
sundropdiamond <Hari@123>
2020-08-08 20:36:33 +0530

    Added main file with routes to home page and alert system page corresponding to urls "/" and "/alert_system" respectively, each using templates provided in the templates directory. "/video_feed" returns only the pure video feed, which is used again in the template for the "/alert_system" url.

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+from camera import VideoCamera
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template("home.html")  # app route "/" delivers whatever is in its function
+
+
+@app.route('/alert_system')
+def alert_system():
+    return render_template("alertsystem.html")
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_jpeg_frame(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+"""mimetype is response media type, for the purpose of having a stream where each part replaces the previous
+ part the multipart/x-mixed-replace content type must be used - here each frame is a part"""
+
+
+# flask uses a generator function to implement streaming
+def gen_jpeg_frame(vid_camera):  # generates jpeg frames from input stream
+    while True:
+        jpeg_byte_array = vid_camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg_byte_array + b'\r\n\r\n')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
