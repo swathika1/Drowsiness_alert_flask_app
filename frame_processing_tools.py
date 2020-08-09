bb2041cdddadb4422419ee81b61f2afdb4379ed9
sundropdiamond <Hari@123>
2020-08-09 10:01:19 +0530

    Added processing for marking face rectangles

--- frame_processing_tools.py
+++ frame_processing_tools.py
@@ -1,13 +1,18 @@
 import cv2
 import imutils
+import dlib
 
 FRAMEWIDTH = 500
 
 
 def process_frame(frame):
     frame = imutils.resize(frame, width=FRAMEWIDTH)
-    # gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    face_detector = dlib.get_frontal_face_detector()  # returns face rectangle(s)
+    face_rectangles = face_detector(gray_frame)  # it is easier to get face rectangle from greyscale image
+    for rect in face_rectangles:
+        cv2.rectangle(img=frame, pt1=(rect.left(), rect.top()), pt2=(rect.right(), rect.bottom()), color=(0, 0, 255), thickness=2)
     return frame
 
 
