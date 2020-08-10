183e94946932aa4ba828498fb9cc766bf5ac51d4
sundropdiamond <Hari@123>
2020-08-10 19:42:51 +0530

    Added VideoGet class

---
+++ get_frame.py
@@ -0,0 +1,23 @@
+import cv2
+from threading import Thread
+
+
+class VideoGet:
+    def __init__(self):
+        self.stream = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        while not self.stopped:
+            if not self.grabbed:
+                self.stop()
+            else:
+                (self.grabbed, self.frame) = self.stream.read()
+
+    def stop(self):
+        self.stopped = True
